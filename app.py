4e0426dbf74587194d2d6c2653fd8e4a03e52665
charles378 <charley.jaru250@gmail.com>
2024-03-02 22:48:57 -0300

    criado

---
+++ app.py
@@ -0,0 +1,22 @@
+import pyautogui
+from time import sleep
+
+#1 clicar no na vegador 906,1053
+pyautogui.click(906,1053,duration=2)
+#2 colar algo na barra de pesgisa 738,259
+pyautogui.click(738,259)
+pyautogui.write('jaca')
+#3 quica para pesquisa 582,253
+pyautogui.click(582,253, duration=2)
+#4 fazer um lupe
+with open('valores.txt', 'r') as arquivo:
+    for linha in arquivo:
+        valor = linha
+	#1 quicar no x 714,110
+        pyautogui.click(714,110, duration=2)
+	#2 colar a streg 443,116
+        pyautogui.click(443,116, duration=2)
+        pyautogui.write(valor)
+	#3 pesquisar 189,106
+        pyautogui.click(189,106, duration=2)
+
